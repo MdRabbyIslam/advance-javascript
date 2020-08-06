47cf1dc6f11a8daeecc5d69d97463eaf7c2bb67f
Rabby-sopno <sopno01737@gmail.com>
2020-08-06 23:31:54 +0600

    bye bye basic and welcome advance js

---
+++ truthyFalsy.js
@@ -0,0 +1,22 @@
+//todo falsy values
+//* false
+//* 0(zero)
+//* ""(empty string)
+//* without assigning variable(like var name; console.log(name) // undefined);
+//* NaN
+//* undefined
+//* null
+
+//todo truthy values
+//* true
+//*numbers are true without zero
+//* " "/ "0 or anything"( not empty string)
+
+//? for experiment
+var name = false;
+
+if (name) {
+    console.log("condition is true")
+} else {
+    console.log("condition is false")
+}
